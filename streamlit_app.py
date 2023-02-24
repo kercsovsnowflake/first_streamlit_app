9ffcf46fb1906baf548cf651d0ab7a7675199cde
kercsovsnowflake <126288650+kercsovsnowflake@users.noreply.github.com>
2023-02-24 22:40:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
-streamlit.text(':green_salad: Kale, Spinach & Rocket Smoothie')
-streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
-streamlit.text(':avocado::bread: Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
